989890d02fae050ff5802c58ad3989bb6c9fec67
olszewskimichal <michalolsz@mat.umk.pl>
2017-04-24 19:04:25 +0200

    porzadki pom.xml

--- src/main/java/com/zespolowka/config/WebSecurityConfig.java
+++ src/main/java/com/zespolowka/config/WebSecurityConfig.java
@@ -1,10 +1,8 @@
 package com.zespolowka.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,7 +17,6 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 @Profile("!test")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
