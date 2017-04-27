62e996719763c51b712950d84e4bbd720c5a5171
olszewskimichal <michalolsz@mat.umk.pl>
2017-04-27 23:06:44 +0200

    aaa

--- src/main/java/com/zespolowka/exceptions/LoginFailedException.java
+++
@@ -1,8 +0,0 @@
-package com.zespolowka.exceptions;
-
-public class LoginFailedException extends RuntimeException {
-	public LoginFailedException(String username) {
-		super(String.format("Błedne logowanie uzytkownika %s", username));
-	}
-
-}
