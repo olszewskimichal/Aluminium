fe418dc9b0dc06837c6b73311183c4af2e8a72d2
olszewskimichal <michalolsz@mat.umk.pl>
2017-04-27 23:15:52 +0200

    aaa

--- src/test/java/com/zespolowka/RegisterControllerTest.java
+++ src/test/java/com/zespolowka/RegisterControllerTest.java
@@ -11,11 +11,10 @@ import com.zespolowka.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -23,8 +22,8 @@ import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(value = "test")
 public class RegisterControllerTest {
-	private static final Logger logger = LoggerFactory.getLogger(RegisterControllerTest.class);
 
 	@Autowired
 	private WebApplicationContext wac;
@@ -68,7 +67,6 @@ public class RegisterControllerTest {
 
 	@Test
 	public void shoud_failed_with_existed_email() throws Exception {
-		logger.info(userService.getAllUsers().toString());
 		mvc.perform(post("/register").param("name", "adam").param("lastName", "malysz").param("email", "aaa3@o2.pl").param("passHash", "zaq1@WSX").param("confirmPassword", "zaq1@WSX")).andExpect(status().isOk()).andExpect(view().name("register")).andExpect(model().errorCount(1));
 	}
 
